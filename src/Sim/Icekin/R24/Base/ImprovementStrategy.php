60654679b69fc26104221711ab65dbcba58d2775
Geert-Jan Selten <geert-jan.selten@nedap.com>
2021-05-07 16:53:55 +0200

    icer sim setup

---
+++ src/Sim/Icekin/R24/Base/ImprovementStrategy.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace OpenDominion\Sim\Icekin\R24\Base;
+
+use OpenDominion\Sim\BaseImprovementStrategy;
+
+class ImprovementStrategy extends BaseImprovementStrategy
+{
+  function investment_strategy() {
+    return [
+      ['keep', 22],
+      ['science', 9],
+      ['walls', 13],
+      ['forges', 13],
+      ['keep', 30],
+    ];
+  }
+}
